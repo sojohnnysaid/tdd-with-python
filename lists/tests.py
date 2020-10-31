5c8f74d11261d9e8504142e30c823f39e0077e42
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-10-31 16:35:57 -0400

    user can submit input box

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,14 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        assert 'A new list item' in response.content.decode()
+        self.assertTemplateUsed(response, 'home.html')
+    
