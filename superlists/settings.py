ae224418a97574ca1dc48d849c9a62dc16961126
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-10-31 11:31:55 -0400

    removed decouple

--- superlists/settings.py
+++ superlists/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from decouple import config
 from pathlib import Path
 
 
@@ -22,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'o6_!yy9uf-add-d2g5fymcob!e*y846us4o44)02(!onmo8s$o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
