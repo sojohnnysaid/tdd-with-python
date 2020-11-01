02a759da4ac30d1b0360af26f25fcf03130ad4f3
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-11-01 14:00:52 -0500

    new URL + view for adding to existing lists. FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 import time
 
-MAX_WAIT = 5
+MAX_WAIT = 10
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
