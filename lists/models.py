57bf9d7e2c6583defb7c27a84b1490018922b640
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-11-01 12:19:22 -0500

    adjust model so that items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
+class List(models.Model):
+    text = models.TextField(default='')
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=CASCADE)
