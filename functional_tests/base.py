25e6d84a4ce70f3c793ad71160a322f7c93c1477
John James Yzaguirre <johnyzaguirre@vassar.edu>
2020-11-03 16:35:56 -0500

    moved functional tests into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,31 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import time
+
+MAX_WAIT = 10
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        opts = webdriver.chrome.options.Options()
+        opts.add_argument('--headless')
+        self.browser = webdriver.Chrome(options=opts)
+        #self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:       
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                assert row_text in [row.text for row in rows]
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.4)
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,36 +1,7 @@
-from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
 from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.chrome.options import Options
-from unittest import skip
-import time
+from selenium.webdriver.common.keys import Keys
 
-MAX_WAIT = 10
-
-class FunctionalTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        #opts = Options()
-        #opts.add_argument('--headless')
-        #self.browser = webdriver.Chrome(options=opts)
-        self.browser = webdriver.Firefox()
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:       
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                assert row_text in [row.text for row in rows]
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.4)
 
 class NewVisitorTest(FunctionalTest):
 
@@ -56,8 +27,7 @@ class NewVisitorTest(FunctionalTest):
         # and now the page lists:
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
+        
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item.
@@ -66,8 +36,7 @@ class NewVisitorTest(FunctionalTest):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
+        
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -119,40 +88,3 @@ class NewVisitorTest(FunctionalTest):
         assert 'Buy milk' in page_text
 
         # Satisfied, they both go back to sleep
-
-class LayoutAndStylingTest(FunctionalTest):
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
-
-        # She starts a new list and sees the input is nicely
-        # Centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
-
-class ItemValidationTest(FunctionalTest):
-
-    @skip
-    def test_cannot_add_empty_list_items(self):
-        # Edith goes to the home page and accidentally tries to submit
-        # an empty list item. She hits Enter on the empty input box
-
-        # The home page refreshes, and there is an error message saying
-        # that list items cannot be blank
-
-        # She tries again with some text for the item, which now works
-
-        # Perversely, she now decides to submit a second blank list item
-
-        # She receives a similar warning on the list page
-
-        # And she can correct it by filling some text in
-        self.fail('write me!')
