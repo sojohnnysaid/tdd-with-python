ab7cff83cff3f7eace3290b698db608663bc21cf
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-11-13 15:48:19 -0500

    added view unit test errors sent back to home page template

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -82,3 +82,10 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text':''})
+        self.asserEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = 'You can\'t have an empty list item'
+        self.assertContains(response, expected_error)
