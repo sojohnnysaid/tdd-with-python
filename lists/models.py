b80afa8c481112c2e3abcf938d655942e835a4cc
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-11-17 16:04:38 -0500

    new form for list items

--- lists/models.py
+++ lists/models.py
@@ -9,7 +9,7 @@ class List(models.Model):
 
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
-    
+
 
 class Item(models.Model):
     text = models.TextField(default='')
