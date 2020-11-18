e91434246c64a1aacb9babd1afcb40874057c96b
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-11-17 21:28:10 -0500

    rename all item input ids and names. still broken

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -52,7 +52,7 @@ class ListViewTest(TestCase):
 
         self.client.post(
             f'/lists/{correct_list.id}/',
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
         )
 
         assert Item.objects.count() == 1
@@ -67,7 +67,7 @@ class ListViewTest(TestCase):
 
         response = self.client.post(
             f'/lists/{correct_list.id}/',
-            data={'item_text': 'A new item for an existing list'}
+            data={'text': 'A new item for an existing list'}
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
@@ -75,7 +75,7 @@ class ListViewTest(TestCase):
     def test_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
         response = self.client.post(
-            f'/lists/{list_.id}/', data={'item_text': ''}
+            f'/lists/{list_.id}/', data={'text': ''}
         )
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
@@ -86,13 +86,13 @@ class ListViewTest(TestCase):
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new/', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new/', data={'text': 'A new list item'})
         assert Item.objects.count() == 1
         new_item = Item.objects.first()
         assert new_item.text == 'A new list item'
 
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new/', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new/', data={'text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
