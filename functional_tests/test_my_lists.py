3e36769a9a4d978be10857a5f5425d6d3dd54d9a
John J Yzaguirre <sojohnnysaid@gmail.com>
2020-11-30 07:27:13 -0500

    test_my_lists: precreate sessions, move login checks into base

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -21,3 +21,13 @@ class MyListsTest(FunctionalTest):
             value=session.session_key,
             path='/',
         ))
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = 'edith@example.com'
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_out(email)
+        
+        # Edith is a logged-in user
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in(email)
